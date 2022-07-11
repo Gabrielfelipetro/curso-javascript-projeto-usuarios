f0630acf19710a07fee6cdd6c26dbb098c151dce
Gabrielfelipetro <gabrielfelipetro@gmail.com>
2022-07-11 11:49:12 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let nome = document.querySelector('#exampleInputName');//document - objeto.querySelector-metodo-(atributo)
 nome.value = 'gabriel'
-nome.style.color = 'green'
+nome.style.color = 'red'
