2ba2caa2945f543582787f8780691910089902b6
Gabrielfelipetro <gabrielfelipetro@gmail.com>
2022-07-11 12:21:50 -0300

    ajustes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let nome = document.querySelector('#exampleInputName');//document - objeto.query
 let gender = document.querySelectorAll('#form-radio [name=gender]:checked');
 let birth = document.querySelector('#exampleInputBirth');
 let country = document.querySelector('#exampleInputCountry');
-let email = document.querySelector('#exampleInputEmail1')
-let password = document.querySelector('#exampleInputPassword1');
+let email = document.querySelector('#exampleInputEmail')
+let password = document.querySelector('#exampleInputPassword');
 let photo = document.querySelector('#exampleInputFile');
 let admin = document.querySelector('#')
