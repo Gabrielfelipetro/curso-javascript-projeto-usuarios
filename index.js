2fd6f0e7405160000598bfe4e80a33383a59d2e8
Gabrielfelipetro <gabrielfelipetro@gmail.com>
2022-07-11 16:34:20 -0300

    inserindo informações no document

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-let gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+let fields = document.querySelectorAll('#form-user-create [name]');
 let user = {};
 
-let field = document.querySelectorAll('#form-user-create [name]');
+function addLine(dataUser){
+    let tr = document.createElement('tr');
 
-field.forEach((field, index)=>{
-    if(field.name == 'gender'){
-        if(field.checked){
+    tr.innerHTML = `
+    <tr>
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.admin}</td>
+    <td>${dataUser.birth}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+  </tr>
+  `;//inner recuper ou atribui valor a um elemento html
+    
+  document.getElementById('table-users').appendChild(tr);//insere um novo nó na estrutura do DOM de um documento
+}
+
+document.getElementById('form-user-create').addEventListener('submit', (event)=>{
+    event.preventDefault(); //para o comportamento padrão do evento
+    
+    fields.forEach((field, index)=>{
+        if(field.name == 'gender'){
+            if(field.checked){
+                user[field.name] = field.value;
+            }
+    
+        } else {
             user[field.name] = field.value;
         }
-
-    } else {
-        user[field.name] = field.value;
-    }
+    });
+    addLine(user)
+    
 });
-
-console.log(user)
