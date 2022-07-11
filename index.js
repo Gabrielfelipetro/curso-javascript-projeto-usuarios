86d84c998c0d1aa2d2156d482ac5973f0e48c42f
Gabrielfelipetro <gabrielfelipetro@gmail.com>
2022-07-11 13:38:28 -0300

    mudei pouca coisa

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let nome = document.querySelector('#exampleInputName');//document - objeto.querySelector-metodo-(atributo)
-let gender = document.querySelectorAll('#form-radio [name=gender]:checked');
-let birth = document.querySelector('#exampleInputBirth');
-let country = document.querySelector('#exampleInputCountry');
-let email = document.querySelector('#exampleInputEmail')
-let password = document.querySelector('#exampleInputPassword');
-let photo = document.querySelector('#exampleInputFile');
-let admin = document.querySelector('#exampleInputAdmin')
+let gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+let user = {};
+
+let field = document.querySelectorAll('#form-user-create [name]');
+
+field.forEach((field, index)=>{
+    if(field.name == 'gender'){
+        if(field.checked){
+            user[field.name] = field.value;
+        }
+
+    } else {
+        user[field.name] = field.value;
+    }
+});
+
+console.log(user)
