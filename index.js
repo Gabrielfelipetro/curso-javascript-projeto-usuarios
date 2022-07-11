f10f9f0076f4c4a0820155e728d50d0bfabb95a1
Gabrielfelipetro <gabrielfelipetro@gmail.com>
2022-07-11 12:19:19 -0300

    desafio correto

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let nome = document.querySelector('#exampleInputName');//document - objeto.querySelector-metodo-(atributo)
-nome.value = 'gabriel'
-nome.style.color = 'red'
+let gender = document.querySelectorAll('#form-radio [name=gender]:checked');
+let birth = document.querySelector('#exampleInputBirth');
+let country = document.querySelector('#exampleInputCountry');
+let email = document.querySelector('#exampleInputEmail1')
+let password = document.querySelector('#exampleInputPassword1');
+let photo = document.querySelector('#exampleInputFile');
+let admin = document.querySelector('#')
